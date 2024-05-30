b5568e20363227490a677ceefed83077d825a70c
Danitarapues2 <dpr.tarapues@yavirac.edu.ec>
2024-05-29 19:57:46 -0500

    Ejercicios

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { Users } from './entity/users.entity';
 
@@ -10,17 +10,33 @@ export class UsersController {
   @Post()
   async create(
     @Body() createUserDto: { name: string; email: string; password: string },
-  ): Promise<Users> {
-    return this.usersService.create(
+  ): Promise<string> {
+    const user = await this.usersService.create(
       createUserDto.name,
       createUserDto.email,
       createUserDto.password,
     );
+
+    return `
+    Nombre: ${user.name}, 
+    Email: ${user.email}, 
+    Contraseña: ${user.password}`;
   }
 
   @Get()
   findAll(): Promise<Users[]> {
     return this.usersService.getAllUsers();
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: number): string {
+    if (id <= 49) {
+      return `El id: ${id} es menor que 50`;
+    } else if (id >= 50 && id <= 99) {
+      return `El id: ${id} es igual o mayor que 50 pero menor a 100 `;
+    } else {
+      return `El Id: ${id} no existe`;
+    }
   }
 
 }
